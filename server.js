f110e258c6fa2acd74fe12f50153508ee3dc14ac
Junzhe Piao <junzhepiao@gmail.com>
2019-03-12 11:58:06 -0700

    edit

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");const path = require("path");
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true })); //make json data readble(postman work)
+app.use(bodyParser.urlencoded({ extended: true })); // make form data readble(web inputs work)
 
 app.set('view engine', 'ejs');
 
